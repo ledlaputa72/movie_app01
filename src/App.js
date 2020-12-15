adf4b03cd87465d46e929c5f8a09b52e568cef9a
ledlaputa <ledlaputa@gmail.com>
2020-12-15 09:56:00 +0900

    'aa'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-import Home from './routers/Home';
 import './App.css';
+import { HashRouter, Route } from "react-router-dom";
+import About from './routers/About';
+import Home from './routers/Home';
+import Navigation from './components/Navigation';
+import Detail from './routers/Detail';
 
 function App() {
-  return <Home />;
+  return (
+  <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About}/>
+    <Route path="/movie-detail" component={Detail}/>
+  </HashRouter>
+  );
 }
 
 export default App;
